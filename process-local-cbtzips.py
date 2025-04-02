5fe13836a176cad14f886242bd797a7a908a659f
azhan92 <alisonxzhang@gmail.com>
2025-04-02 17:51:34 -0400

    Add .zigi info

--- process-local-cbtzips.py
+++ process-local-cbtzips.py
@@ -7,7 +7,7 @@ import filecmp
 import math
 import time
 import datetime
-import xmi
+import xmi, json
 import pandas as pd
 import zipfile
 import yaml
@@ -20,7 +20,9 @@ import subprocess
 
 # Global variables
 CONFIG_FILE = "config.yml"
-SKIP = ["CBT001", "CBT002", "CBT003", "CBT004", "CBT005", "CBT007", "CBT018"] # Don't contain XMI files 
+# Don't contain XMI files 
+SKIP = ["CBT001", "CBT002", "CBT003", "CBT004", "CBT005", "CBT007", 
+        "CBT018", "CBT063", "CBT064", "CBT110", "CBT157", "CBT230"] 
 
 # Configure logfile info
 logfile = f'cbt2git-log-{datetime.datetime.now().strftime("%Y-%j-%H-%M-%S")}'
@@ -40,7 +42,7 @@ logger = logging.getLogger(__name__)
 class XMIObject:
     def __init__(self, name, xmi_file, parent = None):
         self.name = name
-        self.xmi_file = xmi_file            
+        self.xmi_file = xmi_file     
 
         self.xmi_obj = self.extract_xmi()
         if not self.xmi_obj:
@@ -58,30 +60,39 @@ class XMIObject:
         if (self.xmi_obj.is_pds(filename)):
             # if the file is a PDS, set members as new XMIMembers
             self.pds = filename
-            self.create_members() 
+            self.zigidsn = (f'{filename} PO FB 80 32720' + "\n")
+            self.zigispf = {}
+            self.zigispf[self.name] = []
+
+            self.create_members() # get all members of the PDS
 
             if not parent:
-                # If not a nested XMI, create cbt2git log file 
+                # create cbt2git log file if not nested
                 with open(f"{self.repopath}/cbt2git.log", "w") as file:
                     file.writelines(self.loglines)
+                
+                # Create folder for .zigi files 
+                zigi_dir = f"{self.repopath}/.zigi"
+                os.makedirs(zigi_dir, exist_ok = True) 
+                # Create .zigi/dsn file 
+                with open(f"{zigi_dir}/dsn", "w") as file:
+                    file.write(self.zigidsn)
+
+                for f in self.zigispf:
+                    # Create .zigi/name file for parent/any nested XMI's
+                    with open(f"{zigi_dir}/{f}", "w") as file:
+                        file.writelines(self.zigispf[f])
+                
                 self.success = True
             
             else:
                 # If nested, add loglines to parent loglines 
                 parent.loglines.append(str(self.loglines))
+                # Add zigi info 
+                parent.zigidsn += self.zigidsn
+                parent.zigispf[self.name.split('/')[1]] = self.zigispf[self.name]
             
             shutil.rmtree(f"/tmp/{self.pds}") # Remove PDS directory in /tmp
-
-        
-        else:
-            # If not a PDS, add the file as the only member
-            logger.info(f"Adding {filename} as the only member of {self.name}")
-            info = self.xmi_obj.get_file_info_simple(filename) # Get file info 
-            xmi_member = XMIMember(filename, info, self) # Create new member 
-            os.remove(f"/tmp/{filename}{xmi_member.extension}") # Remove file from previous location
-
-        # Remove xmi_file from /tmp folder
-        os.remove(self.xmi_file)      
 
     def extract_xmi(self):
         """Opens the XMI file and extracts its contents."""
@@ -117,17 +128,16 @@ class XMIObject:
 
     def create_members(self):
         """Creates a child XMIObject for each member """
-        xmi_members = self.xmi_obj.get_members(self.pds)
+        # xmi_members = self.xmi_obj.get_members(self.pds)
+        xmi_members = json.loads(self.xmi_obj.get_json()).get('file').get(self.pds).get('members')
         pds_folder = f'{self.repopath}/PDS'
         os.makedirs(pds_folder, exist_ok = True) # Create repo for PDS members
 
-        for m in xmi_members:
+        for m, info in xmi_members.items():
             # Iterate through each member in the PDS
-            info = self.xmi_obj.get_member_info(self.pds, m) # Get member info
-
             if info.get('alias'):
                 # If the member is an alias
-                alias = info.get('alias')
+                alias = self.xmi_obj.get_alias(self.pds, m)
                 try:
                     # Create a symlink 
                     os.symlink(alias, f'{pds_folder}/{m}')
@@ -141,7 +151,10 @@ class XMIObject:
 
             else:
                 # Otherwise create a new member
-                XMIMember(m, info, self)
+                xmi_member = XMIMember(m, info, self)
+                if hasattr(xmi_member, 'ispfline'):
+                    # Add ispf stats if available
+                    self.zigispf[self.name].append(xmi_member.ispfline)
 
                 if info.get('mimetype') == 'application/xmit':
                     # Create new XMIObject if member is an xmi file 
@@ -162,6 +175,35 @@ class XMIMember:
         setattr(self, 'extension', getattr(self, 'extension', '.bin'))
 
         self.move_member()
+
+        # Add ispf stats (not sure if I should do this for all members or not)
+        if not hasattr(self, 'ispf'):
+            self.ispf = {'version': '01.00', 'flags': 0, 'createdate': '1976-06-12T00:00:00.000000', 'modifydate': '1976-06-12T22:18:12.000000', 
+                         'lines': 0, 'newlines': 0, 'modlines': 0, 'user': 'CBT2GIT'}
+        self.ispfstats()
+    
+    def ispfstats(self):
+        # Set creation date 
+        crdat = self.ispf.get('createdate').split('T')[0][2:].replace('-','/')
+
+        # Set modification date
+        if self.ispf.get('modifydate'):
+            mddat = self.ispf.get('modifydate').split('T')[0][2:].replace('-','/')
+            mdtime = self.ispf.get('modifydate').split('T')[1].split('.')[0]
+        else:
+            # If missing 
+            mddat  = '        '
+            mdtime = '        '
+        
+        # Set version
+        v = int(self.ispf.get('version').split('.')[0])
+        m = int(self.ispf.get('version').split('.')[1])
+
+        # Set number of lines 
+        olines = int(self.ispf.get('lines'))
+        nlines = int(self.ispf.get('newlines'))
+
+        self.ispfline =  f"{self.name:<8} {crdat} {mddat} {v:>2} {m:>2} {mdtime} {olines:>5} {nlines:>5} {0:>5} {self.ispf.get('user')}\n"
         
     def move_member(self):
         """Move member file to destination directory based on mimetype"""
@@ -205,9 +247,9 @@ class XMIMember:
             if hasattr(self.parent, 'pds'):
                 # If the member is in a PDS
                 dst = f'{dst_dir}/PDS/{self.name}' 
+
             else:
                 dst = f'{dst_dir}/{self.name}' 
-
             copy_file(src, dst)
         
         logline = f'{datetime.datetime.now()} - Found {self.name}{self.extension} ({self.mimetype}) in {self.pds}, moved to PDS/{self.name}' + '\n'
@@ -245,69 +287,6 @@ def parse_arguments():
                         help=f"Do everything, except remote GitHub actions. (doen't create or updates repos")
     args = parser.parse_args()
     return args
-
-def clean_repos():
-    """Cleans all local (and remote) repos except stage folder if --clean is specified."""
-    # Clean local repositories
-    if only:
-        # Only delete the CBT repo indicated by only
-        os.system(f'rm -rf {cbtfiles}/{only}')
-        os.system(f'rm -rf {repos}/{only}')
-        print(f"Removed local repository: {only}")
-    else:
-        os.system(f'rm -rf {cbtfiles}/*')
-        os.system(f'rm -rf {repos}/*')
-        print("Removed all local repositories.")
-    if not noremote:
-        # Remove remote repositories
-        GIT_REPOS = list(GITHUB_USER.get_repos())
-        total_repos = len(GIT_REPOS)
-        if total_repos == 0:
-            print("No remote repositories to delete.")
-        else:
-            if only:
-                # Only delete the CBT repo indicated by only
-                try:
-                    repo = GITHUB_USER.get_repo(only) 
-                    repo.delete()
-                    print(f"Removed remote repository: {only}")
-                except GithubException as e:
-                    if e.status == 404:
-                        print(f"Repository '{only}' does not exist.")
-                    else:
-                        logger.error(f"Error deleting {only}: {e}")
-                except Exception as e:
-                    logger.error(f"Unexpected error deleting {only}: {e}")
-            else:
-                print(f"Total Repos: {total_repos}")
-                while total_repos > 0:
-                    for repo in GIT_REPOS:
-                        if repo.name[:3] == "CBT":
-                            # Check if the repo is a CBT repo before deleting
-                            rate_used, rate_init = GITHUB_CLIENT.rate_limiting
-                            gracetime = (GITHUB_CLIENT.rate_limiting_resettime-math.floor(time.time())) / 1000
-                            print(f"Deleting {repo.name:8} (gracetime = {gracetime}, ratelimits = {rate_used}/{rate_init})", end=' ', flush=True)
-                            
-                            try:
-                                repo.delete()
-                                # Update the repo list/total count after deletion
-                                GIT_REPOS.remove(repo)
-                                total_repos -= 1
-                            except Exception as e:
-                                logger.error(f"Error deleting {repo.name}: {e}")
-                            
-                            if rate_used >= rate_init * 0.9:  
-                                # Check if close to hitting the rate limit 
-                                time_to_wait = gracetime
-                                # print(f"\nRate limit reached, waiting for {time_to_wait:.2f}s...")
-                                time.sleep(time_to_wait + 1)  
-                            
-                            print(f"Deleted {repo.name}       ", end='\r', flush=True)
-
-                        # Ignore non-CBT repos
-                        else:
-                            GIT_REPOS.remove(repo)
-                            total_repos -= 1
 
 def copy_file(src, dst): 
     """Copies src to dst if new or different.""" 
@@ -420,7 +399,6 @@ def create_git_repo(reponame):
         if rate_used >= rate_init * 0.9:  
             # Check if close to hitting the rate limit 
             time_to_wait = gracetime
-            # print(f"Rate limit reached, waiting for {time_to_wait:.2f}s...")
             time.sleep(time_to_wait + 1)  
         try:
             # Create new repo 
@@ -431,9 +409,7 @@ def create_git_repo(reponame):
             return repourl
         
         except requests.exceptions.ConnectionError as e:
-            # Retry if connection fails 
             wait_time = min(30 * (2 ** retry_count) + random.uniform(0, 5), 300)  
-            # print(f"Connection error: {e}. Retrying in {wait_time:.2f}s...")
             time.sleep(wait_time)
             retry_count += 1
 
@@ -441,7 +417,6 @@ def create_git_repo(reponame):
             if e.status == 403 and "secondary rate limit" in str(e):
                 # Retry if secondary rate limit hit
                 wait_time = min(60 * (2 ** retry_count) + random.uniform(0, 5), 600)
-                # print(f"Secondary rate limit hit. Waiting {wait_time:.2f}s before retrying...")
                 time.sleep(wait_time)
                 retry_count += 1
             else:
@@ -478,7 +453,10 @@ def commit_git_repo(reponame, repourl, remote_name="origin", branch="main"):
         
         # Check if there are changes to commit
         git_status = subprocess.run(["git", "status", "--porcelain"], cwd=repopath, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        if not git_status.stdout.strip():
+        changed_files = [line.split()[-1] for line in git_status.stdout.strip().split("\n") if line]
+        # Filter out cbt2git.log
+        non_log_changes = [f for f in changed_files if os.path.basename(f) != "cbt2git.log"]
+        if not non_log_changes:
             logger.info(f"No updates found for {repourl}.")
             return
 
@@ -529,7 +507,16 @@ if not noremote:
         exit(4)
 
 if args.clean:
-    clean_repos()
+    # Remove local repositories
+    if only:
+        # Only delete the CBT repo indicated by only
+        os.system(f'rm -rf {cbtfiles}/{only}')
+        os.system(f'rm -rf {repos}/{only}')
+        print(f"Removed local repository: {only}")
+    else:
+        os.system(f'rm -rf {cbtfiles}/*')
+        os.system(f'rm -rf {repos}/*')
+        print("Removed all local repositories.")
 
 # Create repo/cbtfile directory if they don't exist
 os.makedirs(repos, exist_ok = True)
@@ -574,13 +561,16 @@ for index, zip in enumerate(sorted(to_process)):
         # Skip for CBT tapes that do not contain XMI files 
         continue
     
-    print(f'{done_bar}{todo_bar} Converting {zip} ({pct}%)', end='\r', flush=True)
+    print(f'{done_bar}{todo_bar} Converting {zip} ({pct}%)           ', end='\r', flush=True)
     logger.info(f"Initialized conversion of {name}.")
     xmi_file = unzip_xmi(zip) # Extract XMI from zip 
 
     if xmi_file:
         # Create an XMIObject to create local repo if xmi was extracted from zip
         xmi_obj = XMIObject(name, xmi_file)
+    else:
+        # Skip if unable to unzip
+        continue
 
     if hasattr(xmi_obj, 'success'):
         # If creating local repo was successful
@@ -592,54 +582,70 @@ for index, zip in enumerate(sorted(to_process)):
 
     if not noremote:
         # Skip the GitHub steps if noremote specified
-        print(f'{done_bar}{todo_bar} Updating GitHub repo {name} ({pct}%)     ', end='\r', flush=True)
+        print(f'{done_bar}{todo_bar} Updating GitHub repo {name} ({pct}%)             ', end='\r', flush=True)
         readme_file(name) # Create README file
         git_attributes(name) # Create .gitattributes file
 
-        rate_used, rate_init = GITHUB_CLIENT.rate_limiting
-        gracetime = (GITHUB_CLIENT.rate_limiting_resettime-math.floor(time.time())) / 1000
-        if rate_used >= rate_init * 0.9:  
-            # Check if close to hitting the rate limit 
-            time_to_wait = gracetime
-            # print(f"Rate limit reached, waiting for {time_to_wait:.2f}s...")
-            time.sleep(time_to_wait + 1)  
-
+        new_repo = False
         try:
-            # Try to get repourl if it exists
-            repourl = GITHUB_USER.get_repo(name).ssh_url
-            logger.info(f"Repo {repourl} already exists.")
+            repo = GITHUB_USER.get_repo(name)
+            repourl = repo.ssh_url
         except GithubException as e:
-            # If repository does not exist, create a new one
             if e.status == 404:
-                repourl = create_git_repo(name)
+                logger.info(f"Repo {repourl} does not exist.")
+                new_repo = True
             else:
                 logger.error(f"Error getting GitHub repo {name}: {e}")
                 continue
-        
-        if not repourl:
-            # Continue if error creating GitHub repo
-            continue
 
+        if not new_repo and args.clean:
+            # Remove repo if clean specified
+            rate_used, rate_init = GITHUB_CLIENT.rate_limiting
+            gracetime = (GITHUB_CLIENT.rate_limiting_resettime-math.floor(time.time())) / 1000
+            if rate_used >= rate_init * 0.9:  
+                # Check if close to hitting the rate limit 
+                time_to_wait = gracetime
+                print(f'{done_bar}{todo_bar} Rate limit reached, waiting for {time_to_wait:.2f}s ({pct}%) ', end='\r', flush=True)
+                time.sleep(time_to_wait + 1)  
+
+            print(f'{done_bar}{todo_bar} Deleting GitHub repo {name} ({pct}%)            ', end='\r', flush=True)
+            try: 
+                repo.delete()
+                new_repo = True
+                time.sleep(10)
+                logger.info(f"Removed Github repo {repourl}.")
+            except Exception as e:
+                logger.error(f"Error deleting Github repo {repourl}: {e}")
+
+        if new_repo:
+            # If repository does not exist, create a new one
+            print(f'{done_bar}{todo_bar} Creating GitHub repo {name} ({pct}%)            ', end='\r', flush=True)
+            repourl = create_git_repo(name)
+            time.sleep(10)
+            if not repourl:
+                # Continue if problems creating git repo
+                continue
+        
         rate_used, rate_init = GITHUB_CLIENT.rate_limiting
         gracetime = (GITHUB_CLIENT.rate_limiting_resettime-math.floor(time.time())) / 1000
         if rate_used >= rate_init * 0.9:  
             # Check if close to hitting the rate limit 
             time_to_wait = gracetime
-            # print(f"Rate limit reached, waiting for {time_to_wait:.2f}s...")
+            print(f'{done_bar}{todo_bar} Rate limit reached, waiting for {time_to_wait:.2f}s ({pct}%) ', end='\r', flush=True)
             time.sleep(time_to_wait + 1)  
-        
+
+        print(f'{done_bar}{todo_bar} Updating GitHub repo {name} ({pct}%)             ', end='\r', flush=True) 
         commit_git_repo(name, repourl) # Commit updates 
 
         rate_used, rate_init = GITHUB_CLIENT.rate_limiting
         gracetime = (GITHUB_CLIENT.rate_limiting_resettime-math.floor(time.time())) / 1000
-        # print(f"Rate critical ({rate_used}/{rate_init}), gracetime={gracetime}")
         if gracetime > 0:
-            # print(f'Sleep for twice the grace time...{gracetime*2:.2f}s')
-            time.sleep(gracetime*2)
+            print(f'{done_bar}{todo_bar} Rate critical ({rate_used}/{rate_init}), gracetime={gracetime} ({pct}%) '   , end='\r', flush=True)
+            time.sleep(gracetime*3)
 
-        if index % 10 == 0 and only == 0:
-            # print("Sleep for 30s...")
-            time.sleep(30)
+        if index != 0 and index % 10 == 0:
+            print(f'{done_bar}{todo_bar} Sleep for 60s ({pct}%)                             ', end='\r', flush=True)
+            time.sleep(60)
     
     logger.info(f"Completed conversion of {name}.")
 
@@ -654,6 +660,3 @@ if not noremote:
 else:
     print(f'All requested CBT files converted and moved to {repos}')
 print(f'This operation took {datetime.time(minute=stop-start)}')
-
-        
-
