2519fa6793eb706ce5efd4d877b726a21414b02b
Wizard of z/OS <wizard@zdevops.com>
2025-04-15 16:56:54 +0200

    Credit where credit due :)

--- process-local-cbtzips.py
+++ process-local-cbtzips.py
@@ -427,7 +427,8 @@ def readme_file(reponame):
     readme = f"""# {reponame}
 Converted to GitHub via [cbt2git](https://github.com/wizardofzos/cbt2git)
 
-This is still a work in progress. GitHub repos will be deleted and created during this period...
+This is still a work in progress. 
+Due to amazing work by Alison Zhang and Jake Choi repos are no longer deleted.
 
 {readme_content}
 """
